ebe1f2f9f40f154177346c0572bc35e80a9ea7ff
infojamesmerchantsolutions-blip <info.jamesmerchantsolutions@gmail.com>
2026-07-10 20:02:53 +0100

    Create binance.provider.ts

---
+++ backend/src/providers/binance.provider.ts
@@ -0,0 +1,89 @@
+import axios from "axios";
+
+import BaseProvider, {
+
+  CreatePaymentInput,
+
+  RefundPaymentInput,
+
+  VerifyPaymentInput,
+
+  ProviderResponse
+
+} from "./base.provider";
+
+export default class BinanceProvider extends BaseProvider {
+
+  readonly name = "binance";
+
+  constructor(
+
+    private readonly apiKey: string,
+
+    private readonly secretKey: string
+
+  ) {
+
+    super();
+
+  }
+
+  async createPayment(
+
+    input: CreatePaymentInput
+
+  ): Promise<ProviderResponse> {
+
+    throw new Error(
+
+      "Not implemented."
+
+    );
+
+  }
+
+  async verifyPayment(
+
+    input: VerifyPaymentInput
+
+  ): Promise<ProviderResponse> {
+
+    throw new Error(
+
+      "Not implemented."
+
+    );
+
+  }
+
+  async refundPayment(
+
+    input: RefundPaymentInput
+
+  ): Promise<ProviderResponse> {
+
+    throw new Error(
+
+      "Not implemented."
+
+    );
+
+  }
+
+  async validateWebhook(
+
+    payload: any,
+
+    signature: string
+
+  ): Promise<boolean> {
+
+    throw new Error(
+
+      "Not implemented."
+
+    );
+
+  }
+
+}
