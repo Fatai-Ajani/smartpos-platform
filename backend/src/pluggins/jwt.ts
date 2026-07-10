c0a10ddc73ae1aee48e3a4f676880ffc2d7109a2
infojamesmerchantsolutions-blip <info.jamesmerchantsolutions@gmail.com>
2026-07-10 01:43:48 +0100

    Update jwt.ts

--- backend/src/pluggins/jwt.ts
+++ backend/src/pluggins/jwt.ts
@@ -1,1 +1,31 @@
+import fp from 'fastify-plugin';
+import { FastifyInstance } from 'fastify';
+import fastifyJwt from '@fastify/jwt';
 
+import { env } from '../config/env';
+
+export const registerJwt = fp(async (app: FastifyInstance) => {
+  await app.register(fastifyJwt, {
+    secret: env.JWT_SECRET,
+    sign: {
+      expiresIn: env.JWT_EXPIRES_IN
+    }
+  });
+
+  app.decorate(
+    'authenticate',
+    async function (request: any, reply: any) {
+      try {
+        await request.jwtVerify();
+      } catch (error) {
+        return reply.send(error);
+      }
+    }
+  );
+});
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    authenticate(request: any, reply: any): Promise<void>;
+  }
+}
