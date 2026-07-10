1dc53688c4ae904da0174510c41b5c7f27be1830
infojamesmerchantsolutions-blip <info.jamesmerchantsolutions@gmail.com>
2026-07-10 03:52:21 +0100

    Create constant.ts

---
+++ backend/src/config/constant.ts
@@ -0,0 +1,25 @@
+export const APP_NAME = "SmartPOS Platform";
+
+export const API_PREFIX = "/api/v1";
+
+export const DEFAULT_PAGE = 1;
+
+export const DEFAULT_LIMIT = 20;
+
+export const MAX_LIMIT = 100;
+
+export const PASSWORD_MIN_LENGTH = 8;
+
+export const PASSWORD_MAX_LENGTH = 128;
+
+export const CACHE_TTL = 300;
+
+export const OTP_EXPIRY = 300;
+
+export const JWT_COOKIE_NAME = "smartpos_access";
+
+export const REFRESH_COOKIE_NAME = "smartpos_refresh";
+
+export const HEALTH_CHECK_ROUTE = "/health";
+
+export const SWAGGER_ROUTE = "/docs";
